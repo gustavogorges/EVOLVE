d3444cad85aadb7e7fbb2125e89cf715c823150a
Deborah Eloise Mattge <deloise@weg.net>
2023-11-10 19:42:59 -0300

    axios funcionado, cards tarefas quase prontos

--- src/app/componentes/tarefa-card-padrao/tarefa-card-padrao.component.ts
+++ src/app/componentes/tarefa-card-padrao/tarefa-card-padrao.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit,Input } from '@angular/core';
+import { BackendEVOLVEService } from 'src/app/service/backend-evolve.service';
 
 @Component({
   selector: 'app-tarefa-card-padrao',
@@ -8,21 +9,52 @@ import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angula
 export class TarefaCardPadraoComponent  {
    valorBarra="0%";
    caminho = "assets/naoVector.svg"
+   caminhoEstrela="assets/estrelaNaoMarcada.svg"
 
-  constructor() { }
+   
+   @Input() id: string = "";
+
+   listaSub = [
+    "subtarefa",
+    "b",
+    "b",
+    
+  ]
+
+  constructor(private service : BackendEVOLVEService ) {
+ 
+   }
 
   ngOnInit(): void {
-    this.valorBarra = 10 +"%"; 
+    this.valorBarra = 60 +"%"; 
 
   
   }
 
-  bomdia(){
-    if (this.caminho == "assets/naoVector.svg"){
-      this.caminho = "assets/vector1.svg"
+  favoritar(){
+    let tarefas;
+    if (this.caminhoEstrela == "assets/estrelaNaoMarcada.svg"){
+      this.caminhoEstrela = "assets/estrelaMarcada.svg"
     } else {
-      this.caminho = "assets/naoVector.svg"
+      this.caminhoEstrela = "assets/estrelaNaoMarcada.svg"
     }
+    this.service.getAllSomething("tarefa")
+    .then(response => {
+    
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    this.service.getOne("tarefa",1)
+    .then(response => {
+    
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
   
   }
--- src/app/tela-tarefa/tela-tarefa.component.ts
+++ src/app/tela-tarefa/tela-tarefa.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaTarefaComponent implements OnInit {
 
+
+  lista = [
+    "a",
+    "b",
+    "C",
+    "D"
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
