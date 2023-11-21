407a95a188e3a73c8d715b64b2e14bf6b36ff7e0
Thiago Alessandro Batista <alebatistathiago@gmail.com>
2023-11-20 22:00:27 -0300

    Implementa rotação no carrosel

--- src/app/componentes/equipes-recentes/equipes-recentes.component.ts
+++ src/app/componentes/equipes-recentes/equipes-recentes.component.ts
@@ -9,13 +9,56 @@ import { BackendEVOLVEService } from 'src/service/backend-evolve.service';
 })
 export class EquipesRecentesComponent implements OnInit {
 
-  listaEquipes:Array<Equipe> = new Array
+  listaEquipes: Array<Equipe> = new Array
+
+  indiceVisualizacao: number = 0
+  listaEquipesVisualizacao: Array<Equipe> = new Array
 
   constructor(private service: BackendEVOLVEService) { }
 
   async ngOnInit(): Promise<void> {
 
     this.listaEquipes = await this.service.getAllSomething("equipe")
+    this.montarListaVisualizacao()
+
+  }
+
+  avancarVisualizacaoCarrosel() {
+    this.indiceVisualizacao++;
+    if (this.indiceVisualizacao >= this.listaEquipes.length) {
+      this.indiceVisualizacao = 0;
+    }
+    this.montarListaVisualizacao()
+  }
+
+  regredirVisualizacaoCarrosel() {
+    this.indiceVisualizacao--;
+    if (this.indiceVisualizacao < 0) {
+      this.indiceVisualizacao = this.listaEquipes.length - 1;
+    }
+    this.montarListaVisualizacao()
+  }
+
+  montarListaVisualizacao() {
+
+    this.listaEquipesVisualizacao[0] = this.listaEquipes[this.indiceVisualizacao]
+    this.listaEquipesVisualizacao[1] = this.listaEquipes[this.indiceVisualizacao + 1]
+    this.listaEquipesVisualizacao[2] = this.listaEquipes[this.indiceVisualizacao + 2]
+
+    if (this.indiceVisualizacao == this.listaEquipes.length - 2) {
+      this.listaEquipesVisualizacao[2] = this.listaEquipes[0]
+    }
+    if (this.indiceVisualizacao == this.listaEquipes.length - 1) {
+      this.listaEquipesVisualizacao[1] = this.listaEquipes[0]
+      this.listaEquipesVisualizacao[2] = this.listaEquipes[1]
+    }
+
+    if (this.listaEquipes.length == 2) {
+      this.listaEquipesVisualizacao.splice(0, 1)
+    }
+    if (this.listaEquipes.length == 1) {
+      this.listaEquipesVisualizacao.splice(1, 2)
+    }
 
   }
 
