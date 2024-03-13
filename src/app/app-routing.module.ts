463c91e9edfa276bfe859c2b890cd4e81ec968a0
Felipe Tomio Maciel <felipetomio@weg.net>
2024-03-12 21:42:50 -0300

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   {
-    path : "",
+    path : "kaique",
     component : LandingPageComponent
   },
   {
