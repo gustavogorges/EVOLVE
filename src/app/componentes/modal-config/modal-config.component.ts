a2429af03ccce598cb5996d519bb5986b5c5eecb
Deborah Eloise Mattge <elomattge@gmail.com>
2024-04-17 20:21:59 -0300

    style:modal config reponsive

--- src/app/componentes/modal-config/modal-config.component.ts
+++ src/app/componentes/modal-config/modal-config.component.ts
@@ -80,6 +80,9 @@ export class ModalConfigComponent implements OnInit {
   cancelPassword(){
     this.disabledPassword=true
   }
+  cancelEmail(){
+    this.disabledEmail=true
+  }
    validarEmail(email: string) {
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return regexEmail.test(email);
