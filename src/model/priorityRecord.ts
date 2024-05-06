f3ccb89a4bea882fcb15f353608d91773769781d
Gustavo Gorges Koch <gustavogk@weg.net>
2024-05-06 16:15:05 -0300

    feat : merge with modal-notifications where already have the task notifications, now starting the project notifications

--- src/model/priorityRecord.ts
+++ src/model/priorityRecord.ts
@@ -1,9 +1,6 @@
 export class PriorityRecord {
     name !: string;
     backgroundColor !: string;
-<<<<<<< HEAD
     value : number = 0;
 }
-=======
-}
->>>>>>> 124a5ecef1aa71ea7e3d059aeefed100b8409c66
+
