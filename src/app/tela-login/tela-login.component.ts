2151c72a4844cb7eff034b24bec36a57de088483
Felipe Tomio Maciel <felipetomio@weg.net>
2024-05-08 15:33:25 -0300

    fix

--- src/app/tela-login/tela-login.component.ts
+++ src/app/tela-login/tela-login.component.ts
@@ -65,16 +65,22 @@ export class TelaLoginComponent implements OnInit {
     return this.userForm.get('password');
   }
   responseData : any 
-  submit() {
-    this.authService.proceedLogin(this.userForm.value).subscribe(result => {
-      if(result!=null){
-        this.responseData = result;
-        this.cookie.setJWTtoken(this.responseData.jwtToken); 
-        this.router.navigate(["/tela-inicial "])
+  async submit() {
+    // this.authService.proceedLogin(this.userForm.value).subscribe(result => {
+    //   if(result!=null){
+    //     this.responseData = result;
+    //     this.cookie.setJWTtoken(this.responseData.jwtToken); 
+    //     this.router.navigate(["/tela-inicial "])
 
-      }
-    })
+    //   }
+    // })
+    let email1 =  this.userForm?.get(['email'])?.value
+    
+    this.usuario = await this.service.getUser(email1);
+    if (this.usuario) {
+      this.router.navigate(['/tela-inicial'], { state: { user: this.usuario } });
   }
+}
   get formControls() {
     return this.passwordForm.controls;
   }
